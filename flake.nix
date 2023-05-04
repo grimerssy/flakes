288524318e8fc20053298cf9967219407e433833
grimerssy <grimerssy@gmail.com>
2023-05-04 20:44:15 +0200

    remove descriptions from flake

--- flake.nix
+++ flake.nix
@@ -1,20 +1,9 @@
 {
-  description = "A collection of flake templates";
-
   outputs = { self }: {
     templates = {
-      default = {
-        path = ./default;
-        description = "Default flake template";
-      };
-      rust = {
-        path = ./rust;
-        description = "Rust flake template";
-      };
-      postgres = {
-        path = ./postgres;
-        description = "Postgresql flake template";
-      };
+      default.path = ./default;
+      rust.path = ./rust;
+      postgres.path = ./postgres;
     };
     defaultTemplate = self.templates.default;
   };
