25c6b5e3ba627c9015a3eff89a6f420c8adc4521
grimerssy <grimerssy@gmail.com>
2023-05-04 20:42:50 +0200

    restructure, use naersk in rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -2,30 +2,43 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    naersk.url = "github:nix-community/naersk";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        buildInputs = with pkgs; [ rust ];
+  outputs = { self, nixpkgs, naersk, rust-overlay, flake-utils }:
+    let
+      forEachSystem = system: rec {
+        packages = {
+          default = naersk'.buildPackage {
+            inherit buildInputs nativeBuildInputs;
+            src = ./.;
+          };
+        };
+        devShells.default = pkgs.mkShellNoCC {
+          inherit buildInputs nativeBuildInputs;
+          packages = with pkgs; [ rust-toolchain ];
+        };
+        buildInputs = with pkgs; [ openssl ];
+        nativeBuildInputs = with pkgs;
+          [ pkg-config ] ++ lib.optionals stdenv.isDarwin
+          (with darwin.apple_sdk.frameworks; [
+            CoreServices
+            Security
+            SystemConfiguration
+          ]);
 
         pkgs = import nixpkgs { inherit system overlays; };
         overlays = [
+          (import rust-overlay)
           (self: super: {
-            rust = (import nixpkgs {
-              inherit system;
-              overlays = [ (import rust-overlay) ];
-            }).rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" ];
-            };
+            rust-toolchain = super.rust-bin.stable.latest.default;
           })
         ];
-      in {
-        devShells.default = pkgs.mkShellNoCC {
-          buildInputs = buildInputs ++ (with pkgs;
-            lib.optionals stdenv.isDarwin
-            (with darwin.apple_sdk.frameworks; [ ]));
+        naersk' = pkgs.callPackage naersk rec {
+          cargo = rustc;
+          rustc = pkgs.rust-toolchain;
         };
-      });
+      };
+    in flake-utils.lib.eachDefaultSystem forEachSystem;
 }
