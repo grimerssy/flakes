21a8a9fec2b22aeb20c8df5511bc4918b9e50f2f
grimerssy <grimerssy@gmail.com>
2023-05-05 12:24:46 +0200

    remove default template

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
   outputs = { self }: {
     templates = {
-      default.path = ./default;
       rust.path = ./rust;
       postgres.path = ./postgres;
     };
-    defaultTemplate = self.templates.default;
   };
 }
