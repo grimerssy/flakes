8f21e7bb3dfad3bf7faaa38592bf5d74d09b9d09
grimerssy <grimerssy@gmail.com>
2023-05-06 14:47:39 +0200

    make flake.nix dynamic

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
-  outputs = { self }: {
-    templates = {
-      rust.path = ./rust;
-      postgres.path = ./postgres;
+  outputs = { self }:
+    let
+      files = builtins.readDir ./.;
+      directories = builtins.filter (file: files.${file} == "directory")
+        (builtins.attrNames files);
+    in {
+      templates = builtins.listToAttrs (builtins.map (dir: {
+        name = dir;
+        value.path = ./${dir};
+      }) directories);
     };
-  };
 }
