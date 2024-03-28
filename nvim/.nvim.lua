8235e5c44086d8dde22f72a6a045f674d66b60f5
Stanislav Stoyanov <grimerssy@gmail.com>
2024-03-28 16:12:02 +0100

    add .nvim.lua with lspconfig override as a template

---
+++ nvim/.nvim.lua
@@ -0,0 +1,23 @@
+local ok, lspconfig = pcall(require, "lspconfig")
+if not ok then
+    return
+end
+
+local servers = {}
+
+for server, override in pairs(servers) do
+    local config = override
+    if lspconfig[server].manager then
+        local existing_config = lspconfig[server].manager.config
+        config = vim.tbl_deep_extend("force", existing_config, override)
+        for _, cb in ipairs({ "on_init", "on_attach" }) do
+            if existing_config[cb] and override[cb] then
+                config[cb] = function(...)
+                    existing_config[cb](...)
+                    override[cb](...)
+                end
+            end
+        end
+    end
+    lspconfig[server].setup(config)
+end
