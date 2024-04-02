af8e0c004a624d625f916a9032a34b2a1b154abd
Stanislav Stoyanov <grimerssy@gmail.com>
2024-04-02 23:50:53 +0200

    set latest version to rust flake

--- flake-rust/flake.nix
+++ flake-rust/flake.nix
@@ -14,7 +14,7 @@
       {
         devShells.default = pkgs.mkShellNoCC {
           packages = with pkgs; [
-            (rust-bin.stable."1.75.0".default.override {
+            (rust-bin.stable.latest.default.override {
               extensions = [ "rust-src" "clippy" "rust-analyzer" ];
             })
           ];
