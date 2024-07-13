fba6ec048249d65bba967d0d2450a44d52ea3581
Stanislav Stoyanov <grimerssy@gmail.com>
2024-07-13 21:20:30 +0200

    simplify lua setup in nvim lsp template

--- nvim/lsp/.nvim.lua
+++ nvim/lsp/.nvim.lua
@@ -5,31 +5,8 @@ end
 
 local servers = {}
 
---- @return unknown
-local function extend_config(old, new)
-    if type(new) ~= "table" then
-        return new
-    end
-    if new._append then
-        for _, v in ipairs(new._append) do
-            table.insert(old, v)
-        end
-        return old
-    end
-    if new._compose then
-        return function(...)
-            old(...)
-            return new._compose(...)
-        end
-    end
-    for k in pairs(new) do
-        old[k] = extend_config(old[k], new[k])
-    end
-    return old
-end
-
-for server, override in pairs(servers) do
-    local manager = lspconfig[server].manager or {}
-    local config = extend_config(manager.config or {}, override)
-    lspconfig[server].setup(config)
+for server, delta in pairs(servers) do
+    local old = (lspconfig[server].manager or {}).config or {}
+    local new = vim.tbl_deep_extend("force", old, delta(old))
+    lspconfig[server].setup(new)
 end
